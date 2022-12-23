c56e91379bf69fe6b297d509f82dcf62ed175aac
Henry Sullivan <sullivanbhenry@gmail.com>
2022-12-23 08:36:02 -0800

    set up database in mysql, and assign properties to models based on database

---
+++ HairSalon/Models/Client.cs
@@ -0,0 +1,11 @@
+namespace HairSalon.Models
+{
+  public class Client
+  {
+     public string Name { get; set; }
+    public int StylistId { get; set; }
+    public int ClientId { get; set; }
+    public Stylist Stylist { get; set; }
+
+  }
+}
